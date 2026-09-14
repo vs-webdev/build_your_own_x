b5244b0c866fc42132b82c80305f4c46d656ac86
Vikas Solanki <solankivikas066@gmail.com>
2026-09-14 23:06:40 +0530

    feat: add initial state and reducer function with create and update note actions

---
+++ redux/index.js
@@ -0,0 +1,58 @@
+const CREATE_NOTE = 'CREATE_NOTE';
+const UPDATE_NOTE = 'UPDATE_NOTE';
+
+const initialState = {
+  nextNoteId: 1,
+  notes: {}
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_NOTE: {
+      const id = state.nextNoteId;
+      const newNote = {
+        id,
+        content: ''
+      };
+      return {
+        ...state,
+        nextNoteId: id + 1,
+        notes: {
+          ...state.notes,
+          [id]: newNote
+        }
+      };
+    }
+    case UPDATE_NOTE: {
+      const {id, content} = action;
+      const editedNote = {
+        ...state.notes[id],
+        content
+      };
+      return {
+        ...state,
+        notes: {
+          ...state.notes,
+          [id]: editedNote
+        }
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const state0 = reducer(undefined, {
+  type: CREATE_NOTE
+});
+
+const state1  = reducer(state0, {
+  type: UPDATE_NOTE,
+  id: 1,
+  content: 'Hello, world!'
+});
+
+ReactDOM.render(
+  <pre>{JSON.stringify(state1, null, 2)}</pre>,
+  document.getElementById('root')
+);
